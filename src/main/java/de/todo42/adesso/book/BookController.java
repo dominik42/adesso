092ccb77ba71ca988cd838e79009283c29cce47c
Dominik Hirt <hirt@todo42.de>
2019-05-23 15:32:51 +0200

    TASK services extended

--- src/main/java/de/todo42/adesso/book/BookController.java
+++ src/main/java/de/todo42/adesso/book/BookController.java
@@ -1,5 +1,8 @@
 package de.todo42.adesso.book;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +15,13 @@ public class BookController {
 
     public static final String REQUEST_URL = "/book";
     
+    @Autowired
+    private BookService bookService;
+    
+    
     @GetMapping
-    public Book getAllBooks() {
-        Book book = Book.builder()
-                .title("Spring Boot 2")
-                .author("Michael Simons")
-                .isbn("1234567890123")
-                .build();
-        return book;
+    public Collection<Book> getAllBooks() {
+        return bookService.loadAllBooks();
     }
     
     @GetMapping(path = "/{isbn}")
