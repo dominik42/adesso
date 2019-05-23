eed06b0e50283c1d55188019011f9446cf718e97
Dominik Hirt <hirt@todo42.de>
2019-05-23 11:09:34 +0300

    init

---
+++ src/main/java/de/todo42/adesso/AdessoApplication.java
@@ -0,0 +1,13 @@
+package de.todo42.adesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdessoApplication.class, args);
+	}
+
+}
