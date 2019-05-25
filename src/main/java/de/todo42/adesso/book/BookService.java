82ce1a0daa80c101248f04094ee65497d9dd9f3d
Dominik Hirt <hirt@todo42.de>
2019-05-25 07:32:07 +0200

    alternative query with jdbcTemplate

--- src/main/java/de/todo42/adesso/book/BookService.java
+++ src/main/java/de/todo42/adesso/book/BookService.java
@@ -1,9 +1,12 @@
 package de.todo42.adesso.book;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 import lombok.NonNull;
@@ -30,10 +33,22 @@ public class BookService {
     }
     
     public Book loadByIsbn(String isbn) {
-//        String sql = "SELECT * FROm books WHERE isbn = '" + isbn + "'";
-//        Book book = jdbcTemplate.queryForObject(sql, Book.class);
-        return bookRepository.getByIsbn(isbn);
-        //return book;
+        String sql = "SELECT * FROM books WHERE isbn = '" + isbn + "'";
+        Book book = jdbcTemplate.queryForObject(sql, new RowMapper<Book>() {
+
+            @Override
+            public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Book book = Book.builder()
+                        .title(rs.getString("title"))
+                        .author(rs.getString("author"))
+                        .isbn(rs.getString("isbn"))
+                        .build();
+                return book;
+            }
+            
+        });
+        return book;
+        //return bookRepository.getByIsbn(isbn);
     }
 
     
